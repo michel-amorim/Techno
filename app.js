aefd2f216d75e9c9d6e76c06a1677be1180fc51d
Michel Amorim <michelmsa7799@gmail.com>
2022-08-30 08:52:54 -0300

    feat: style carrinho

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    mensagemAlerta: "Item adicionado",
+    alertaAtivo: false,
+    carrinhoAtivo: false,
   },
 
   filters: {
@@ -51,30 +54,50 @@ const vm = new Vue({
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) this.produto = false;
     },
-
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) this.carrinhoAtivo = false;
+    },
     adicionarItem() {
       this.produto.estoque--;
       const { id, nome, preco } = this.produto;
       this.carrinho.push({ id, nome, preco });
+      this.alerta(`${nome} adicionado ao carrinho`);
     },
 
     removerItem(index) {
       this.carrinho.splice(index, 1);
     },
-    chegarLocalStorage() {
-      if (window.localStorage.carrinho) {
+    checarLocalStorage() {
+      if (window.localStorage.carrinho)
         this.carrinho = JSON.parse(window.localStorage.carrinho);
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      }, 1500);
+    },
+    router() {
+      const hash = document.location.hash;
+      if (hash) {
+        this.fetchProduto(hash.replace("#", ""));
       }
     },
   },
   watch: {
+    produto() {
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
+    },
     carrinho() {
-      window.localStorage.carinho = JSON.stringify(this.carrinho);
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
     },
   },
-
   created() {
     this.fetchProdutos();
-    this.chegarLocalStorage();
+    this.checarLocalStorage();
+    this.router();
   },
 });
